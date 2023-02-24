d671785c98d50a28cad737842e05d51ca4d01581
Azamjon <Azamjon_017@gmail.com>
2023-02-24 09:59:43 +0500

    ADD

---
+++ git.py
@@ -0,0 +1,14 @@
+
+
+
+def find(n: int or float) -> int or float:
+    
+    if n >= 0:
+        return False
+    else:
+        return True    
+n = int(input("Raqam kiriting (int):"))
+print(find(n))
+
+
+
